a8ba393b609217e33a15fb4be4136a3d93c1f422
Echezona Oji <echyth@yahoo.com>
2024-05-25 19:45:17 +1000

    feat (task 1): initial commit

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
+
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data}`);
+  process.exit();
+});
