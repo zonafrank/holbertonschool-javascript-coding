724c218d51c355af4322c0a1ba29299b38a0b3d8
Echezona Oji <echyth@yahoo.com>
2024-05-25 23:41:59 +1000

    feat (task 5): initial commit

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const url = require('url');
+const {
+  promises: { readFile },
+} = require('fs');
+
+const fileName = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url).pathname;
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (reqUrl === '/') {
+    res.write('Hello Holberton School');
+    res.end();
+  } else if (reqUrl === '/students') {
+    let text = 'This is the list of our students';
+    readFile(fileName)
+      .then((fileBuffer) => {
+        const fileData = fileBuffer.toString();
+        const lines = fileData.split('\n').slice(1);
+        const result = {};
+        let studentCount = 0;
+
+        for (const line of lines) {
+          if (line.length !== 0) {
+            studentCount += 1;
+            const tokens = line.split(',');
+            const firstName = tokens[0];
+            const subject = tokens[3];
+
+            if (!(subject in result)) {
+              result[subject] = { count: 1, students: [firstName] };
+            } else {
+              result[subject].count += 1;
+              result[subject].students.push(firstName);
+            }
+          }
+        }
+
+        text += `\nNumber of students: ${studentCount}`;
+
+        for (const field in result) {
+          if (Object.getOwnPropertyNames(result).includes(field)) {
+            text += `\nNumber of students in ${field}: ${
+              result[field].count
+            }. List: ${result[field].students.join(', ')}`;
+          }
+        }
+        res.write(text);
+        res.end();
+      })
+      .catch(() => {
+        res.write('Cannot load the database');
+        res.end();
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
