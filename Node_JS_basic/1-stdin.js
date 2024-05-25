537aac9b679be6d8fd1f0ba3a7aca6b77dbba43b
Echezona Oji <echyth@yahoo.com>
2024-05-25 20:37:49 +1000

    fix (task 1): modifications for checker failing test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  process.stdin.write(`Your name is: ${data.toString().trim()}\n`);
+  console.log(`Your name is: ${data.toString().trim()}\n`);
   console.log('This important software is now closing');
   process.exit();
 });
