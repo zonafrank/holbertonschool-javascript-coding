8e7a01019bbf0666848eb33f6fa4da7920beb933
Echezona Oji <echyth@yahoo.com>
2024-05-26 23:00:04 +1000

    fix (task 8): trying alternative StudentController for checker tests

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,40 +1,33 @@
-// const { readDatabase } = require("../utils");
-import readDatabase from "../utils";
-
-// const fileName = process.argv[2];
-// const fileName = './database.csv';
+import readDatabase from '../utils';
 
 class StudentsController {
   static async getAllStudents(request, response) {
     try {
       const data = await readDatabase(process.argv[2]);
       const dataKeys = Object.keys(data).sort();
-      let responseText = "This is the list of our students";
+      let responseText = 'This is the list of our students';
       for (const k of dataKeys) {
         const students = data[k];
-        const studentsListText = students.join(", ");
+        const studentsListText = students.join(', ');
         responseText += `\nNumber of students in ${k}: ${students.length}. List: ${studentsListText}`;
       }
       response.status(200).send(responseText);
     } catch (error) {
-      response.writeHead(500);
-      response.end("Cannot load the database");
+      response.status(500).end('Cannot load the database');
     }
   }
 
   static async getAllStudentsByMajor(request, response) {
     try {
       const { major } = request.params;
-      if (major !== "CS" && major !== "SWE") {
-        response.writeHead(500);
-        return response.end("Major parameter must be CS or SWE");
+      if (major !== 'CS' && major !== 'SWE') {
+        return response.status(500).end('Major parameter must be CS or SWE');
       }
 
       const data = await readDatabase(process.argv[2]);
-      return response.status(200).send(`List: ${data[major].join(", ")}`);
+      return response.status(200).send(`List: ${data[major].join(', ')}`);
     } catch (error) {
-      response.writeHead(500);
-      return response.end("Cannot load the database");
+      return response.status(500).end('Cannot load the database');
     }
   }
 }
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,38 +1,36 @@
 const {
-  promises: { readFile }
-} = require("fs");
+  promises: { readFile },
+} = require('fs');
 
 // import { readFile } from "node:fs/promises";
 
 async function readDatabase(filePath) {
-  return new Promise((resolve, reject) =>
-    readFile(filePath)
-      .then((fileBuffer) => {
-        const fileData = fileBuffer.toString();
-        const lines = fileData.split("\n").slice(1);
-        const result = {};
+  return new Promise((resolve, reject) => readFile(filePath)
+    .then((fileBuffer) => {
+      const fileData = fileBuffer.toString();
+      const lines = fileData.split('\n').slice(1);
+      const result = {};
 
-        for (const line of lines) {
-          if (line.length !== 0) {
-            const tokens = line.split(",");
-            const firstName = tokens[0];
-            const subject = tokens[3];
+      for (const line of lines) {
+        if (line.length !== 0) {
+          const tokens = line.split(',');
+          const firstName = tokens[0];
+          const subject = tokens[3];
 
-            if (!(subject in result)) {
-              result[subject] = [firstName];
-            } else {
-              result[subject].push(firstName);
-            }
+          if (!(subject in result)) {
+            result[subject] = [firstName];
+          } else {
+            result[subject].push(firstName);
           }
         }
+      }
 
-        resolve(result);
-      })
-      .catch((err) => {
-        console.log(err);
-        reject(err);
-      })
-  );
+      resolve(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      reject(err);
+    }));
 }
 
 export default readDatabase;
