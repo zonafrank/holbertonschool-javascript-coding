722c7c11e2f6f0307d089e35b38c7955d15d0e2e
Echezona Oji <echyth@yahoo.com>
2024-05-26 22:47:19 +1000

    fix (task 8): changes to meet checker requirements

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,43 +1,42 @@
 // const { readDatabase } = require("../utils");
-import readDatabase from '../utils';
+import readDatabase from "../utils";
 
 // const fileName = process.argv[2];
-const fileName = './database.csv';
+// const fileName = './database.csv';
 
 class StudentsController {
   static async getAllStudents(request, response) {
-    console.log(`process.argv: ${process.argv}`);
     try {
-      const data = await readDatabase(fileName);
+      const data = await readDatabase(process.argv[2]);
       const dataKeys = Object.keys(data).sort();
       response.status(200);
-      response.write('This is the list of our students');
+      response.write("This is the list of our students");
       for (const k of dataKeys) {
         const students = data[k];
-        const studentsListText = students.join(', ');
+        const studentsListText = students.join(", ");
         const text = `\nNumber of students in ${k}: ${students.length}, List: ${studentsListText}`;
         response.write(text);
       }
       response.end();
     } catch (error) {
       response.writeHead(500);
-      response.end('Cannot load the database');
+      response.end("Cannot load the database");
     }
   }
 
   static async getAllStudentsByMajor(request, response) {
     try {
       const { major } = request.params;
-      if (major !== 'CS' && major !== 'SWE') {
+      if (major !== "CS" && major !== "SWE") {
         response.writeHead(500);
-        response.end('Major parameter must be CS or SWE');
+        response.end("Major parameter must be CS or SWE");
       }
 
-      const data = await readDatabase(fileName);
-      return response.status(200).send(`List: ${data[major].join(', ')}`);
+      const data = await readDatabase(process.argv[2]);
+      return response.status(200).send(`List: ${data[major].join(", ")}`);
     } catch (error) {
       response.writeHead(500);
-      return response.end('Cannot load the database');
+      return response.end("Cannot load the database");
     }
   }
 }
